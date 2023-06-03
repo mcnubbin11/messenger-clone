cd48eca015fdce27d261e25db7f582990d20828d
jacobtobias <jacob@onprem.com>
2023-06-03 15:08:52 -0500

    Started auth form component

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+
+type Variant = 'LOGIN' | 'REGISTER';
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
+    } else {
+      setVariant('LOGIN');
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === 'REGISTER') {
+      // Axios register
+    }
+
+    if (variant === 'LOGIN') {
+      // NextAuth login
+    }
+  };
+
+  return <div>Auth Form!</div>;
+};
+
+export default AuthForm;
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import AuthForm from './components/AuthForm';
 
 export default function Home() {
   return (
@@ -22,7 +23,20 @@ export default function Home() {
           className='mx-auto w-auto'
           src='/images/logo.png'
         />
+        <h2
+          className='
+            mt-6
+            text-center
+            text-3xl
+            font-bold
+            tracking-tight
+            text-gray-900
+          '
+        >
+          Sign in to your account
+        </h2>
       </div>
+      <AuthForm />
     </div>
   );
 }
